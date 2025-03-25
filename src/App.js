83226b658f5cb935c50d21ebd1e7bd041c62d155
crijanp <srij89995@gmail.com>
2025-03-25 23:03:54 +0545

    component call

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FileEncryptionTool from './components/FileEncryptionTool';
+
+function App() {
+  return (
+    <div className="App">
+      <FileEncryptionTool/>
+    </div>
+  );
+}
+
+export default App;
