4d186155e1ea37ad13847c8289ccead445cd2d4b
crijanp <srij89995@gmail.com>
2025-03-25 23:02:09 +0545

    component1  initial commit

---
+++ src/components/FileEncryptionTool.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/FileEncryption.module.css';
+
+function FileEncryptionTool() {
+  return (
+    <div className={styles.container}>
+      <h1>File Encryption Tool</h1>
+    </div>
+  );
+}
+
+export default FileEncryptionTool;
